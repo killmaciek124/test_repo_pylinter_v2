5d3f00cb6b2bc2e448b5642474cd6b7ff5e22aa5
Maciej Maziuk <86524345+killmaciek124@users.noreply.github.com>
2022-01-22 22:03:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,6 @@ import os
 
 
 def add(a, b) -> int:
-    return math.floor(a + b)
+    return math.floor(a * b)
 
 
-def to_sentence(s) -> str:
-    s = s.capitalize()
-
-    if s.endswith('.'):
-        return s
-    else:
-        return s + '.'
